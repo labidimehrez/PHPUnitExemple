a46ad0b0bcf4348471af00e1e4c1286377e7294f
mehrez.labidi <mehrez.labidi@esprit.tn>
2019-06-02 19:24:51 +0200

    test unitaire basique

---
+++ tests/AppBundle/Entity/CalculatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Tests\AppBundle\Entity;
+use AppBundle\Entity\Calculator;
+use PHPUnit\Framework\TestCase;
+/**
+ * Description of Calculator
+ *
+ * @author mehrez
+ */
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(30, 12);
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals(42, $result);
+    }
+}
